c5941bd9d85290485c6e2dbbe353f305c57e9e35
tkovs <joao.rodrigues@arapiraca.ufal.br>
2019-01-06 09:48:32 -0300

    Adds the note about let of docs

---
+++ courses/handbook-typescriptlang/01-basic-types/13-note-about-let.ts
@@ -0,0 +1,8 @@
+// A note about Let
+
+// You may’ve noticed that so far, we’ve been using the let keyword instead of
+// JavaScript’s var keyword which you might be more familiar with. The let
+// keyword is actually a newer JavaScript construct that TypeScript makes
+// available. We’ll discuss the details later, but many common problems in
+// JavaScript are alleviated by using let, so you should use it instead of var
+// whenever possible.
