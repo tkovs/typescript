9f57523e02081d547629f1223165a0b69b727a93
tkovs <joao.rodrigues@arapiraca.ufal.br>
2019-01-10 07:33:35 -0300

    Traslates the array docs into pr-br

--- courses/handbook-typescriptlang/01-basic-types/04-array.ts
+++ courses/handbook-typescriptlang/01-basic-types/04-array.ts
@@ -1,9 +1,10 @@
 // Array
 
-// TypeScript, like JavaScript, allows you to work with arrays of values. Array
-// types can be written in one of two ways. In the first, you use the type of
-// the elements followed by [] to denote an array of that element type:
+// O Typescript, assim como o Javascript, permite a você trabalhar com arrays
+// de valores. Tipos Array podem ser escritos de duas formas. Na primeira, você
+// usa o tipo dos elementos seguido de [] para denotar um array do tipo do
+// elemento.
 let list: number[] = [1, 2, 3];
 
-// The second way uses a generic array type, Array<elemType>:
+// Na segunda forma, você usa um tipo genérico de Array, Array<elemType>:
 let list2: Array<number> = [1, 2, 3];
