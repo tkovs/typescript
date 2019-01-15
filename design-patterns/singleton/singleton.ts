ec4bef66faf8b78ff5267d4b81a0def8832740ce
tkovs <joao.rodrigues@arapiraca.ufal.br>
2019-01-14 23:44:04 -0300

    Renames the singleton object variable name to instance

--- design-patterns/singleton/singleton.ts
+++ design-patterns/singleton/singleton.ts
@@ -1,20 +1,20 @@
 namespace SingletonPattern {
     export class Singleton {
         // Variável que guarda a única instância dese objeto
-        private static singleton: Singleton;
+        private static instance: Singleton;
 
         private constructor() { }
 
-        // Método de criação de um singleton, caso ele não exista
+        // Método de criação de um singleton, caso ele não existia
         public static getInstance(): Singleton {
             // Checa existencia do singleton
-            if (!Singleton.singleton) {
+            if (!Singleton.instance) {
                 // Se nenhum singleton foi criado, instancia o primeiro
-                Singleton.singleton = new Singleton();
+                Singleton.instance = new Singleton();
             }
 
             // Retorna o singleton (já instanciado)
-            return Singleton.singleton;
+            return Singleton.instance;
         }
     }
 }
