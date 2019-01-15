509b3697ae45e955466ac55f76acfad15757a59d
tkovs <joao.rodrigues@arapiraca.ufal.br>
2019-01-14 23:41:22 -0300

    Adds some comments about the singleton pattern

--- design-patterns/singleton/singleton.ts
+++ design-patterns/singleton/singleton.ts
@@ -1,14 +1,19 @@
 namespace SingletonPattern {
     export class Singleton {
+        // Variável que guarda a única instância dese objeto
         private static singleton: Singleton;
 
         private constructor() { }
 
+        // Método de criação de um singleton, caso ele não exista
         public static getInstance(): Singleton {
+            // Checa existencia do singleton
             if (!Singleton.singleton) {
+                // Se nenhum singleton foi criado, instancia o primeiro
                 Singleton.singleton = new Singleton();
             }
 
+            // Retorna o singleton (já instanciado)
             return Singleton.singleton;
         }
     }
